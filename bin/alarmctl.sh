81c0db57dcd10351134138699adf57e21849ba17
MiguSchweiz <miguschweiz@bluewin.ch>
2020-04-17 14:05:58 +0200

    -

--- bin/alarmctl.sh
+++ bin/alarmctl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 slum=`cat /home/pi/piclck/www/alarms | awk -F ";" '{ print $5 }'`
 if [ "$1" == "stop" ];then
-    pkill vlc
     pkill playStream.sh
+    pkill vlc
     rm /home/pi/.alarmOn
     rm /home/pi/.slum
 elif [ "$1" == "slum" ];then
@@ -10,8 +10,8 @@ elif [ "$1" == "slum" ];then
         exit
     fi
     touch /home/pi/.slum
-    pkill vlc
     pkill playStream.sh
+    pkill vlc
     sleep $slum
     /home/pi/piclck/bin/playStream.sh &
     rm /home/pi/.slum
