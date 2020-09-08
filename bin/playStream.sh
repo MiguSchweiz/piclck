8426752ad7a6bb1e7e1be26360b385b661ab87ee
MiguSchweiz <miguschweiz@bluewin.ch>
2020-09-08 14:53:33 +0200

    -

--- bin/playStream.sh
+++ bin/playStream.sh
@@ -27,22 +27,22 @@ elif [ $st -eq 5 ];then
 fi
 
 
-sudo echo connect 00:11:2D:5E:52:F9 |bluetoothctl
+sudo echo connect EC:81:93:55:39:D0 |bluetoothctl
 
 cvlc --volume-step=256 --loop $stat 2>&1 >/dev/null &
 
 sleep 5
 
 while true; do
-    sudo echo info 00:11:2D:5E:52:F9 |bluetoothctl|grep Connected|grep yes >/dev/null
+    sudo echo info EC:81:93:55:39:D0 |bluetoothctl|grep Connected|grep yes >/dev/null
     if [ $? -ne 0 ];then
-        if [ $ecnt -eq 2 ];then
+        if [ $ecnt -eq 1000 ];then
             echo "restart bluetooth..."
             sudo systemctl restart bluetooth
             ecnt=0
         fi
         pkill vlc
-        sudo echo connect 00:11:2D:5E:52:F9 |bluetoothctl
+        sudo echo connect EC:81:93:55:39:D0|bluetoothctl
         cvlc --volume-step=256 --loop $stat 2>&1 >/dev/null &
         ((ecnt=ecnt+1))
     fi
